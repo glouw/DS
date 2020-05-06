13a337a90936c4077c092a9f9b80881f911de8c3
Gustav Louw <airglouw@gmail.com>
2020-05-06 15:08:47 -0700

    little rename

--- List.c
+++ List.c
@@ -2,26 +2,26 @@
 
 #include <stdlib.h>
 
-typedef struct ListNode ListNode;
+typedef struct Node Node;
 
-struct ListNode
+struct Node
 {
     void* data;
-    struct ListNode* next;
-    struct ListNode* prev;
+    struct Node* next;
+    struct Node* prev;
 };
 
 struct List
 {
-    struct ListNode* head;
-    struct ListNode* tail;
+    struct Node* head;
+    struct Node* tail;
     void (*destruct)(void* data);
     int size;
 };
 
-static ListNode* ListNode_Construct(void* data)
+static Node* Node_Construct(void* data)
 {
-    ListNode* node = calloc(1, sizeof(*node));
+    Node* node = calloc(1, sizeof(*node));
     node->data = data;
     return node;
 }
@@ -35,10 +35,10 @@ List* List_Construct(void (*destruct)(void*))
 
 void List_Destruct(List** list)
 {
-    ListNode* node = (*list)->head;
+    Node* node = (*list)->head;
     while(node)
     {
-        ListNode* next = node->next;
+        Node* next = node->next;
         if((*list)->destruct)
             (*list)->destruct(node->data);
         free(node);
@@ -53,10 +53,10 @@ int List_GetSize(List* list)
     return list->size;
 }
 
-static void List_Remove(List* list, ListNode* node)
+static void List_Remove(List* list, Node* node)
 {
-    ListNode* prev = node->prev;
-    ListNode* next = node->next;
+    Node* prev = node->prev;
+    Node* next = node->next;
     if(list->destruct)
         list->destruct(node->data);
     free(node);
@@ -76,7 +76,7 @@ void List_Delete(List* list, const int index)
     if(index < list->size)
     {
         int i = 0;
-        for(ListNode* node = list->head; node; node = node->next)
+        for(Node* node = list->head; node; node = node->next)
         {
             if(index == i)
             {
@@ -90,10 +90,10 @@ void List_Delete(List* list, const int index)
 
 void List_DeleteMatch(List* list, bool (*match)(void*))
 {
-    ListNode* node = list->head;
+    Node* node = list->head;
     while(node)
     {
-        ListNode* next = node->next;
+        Node* next = node->next;
         if(match(node->data))
             List_Remove(list, node);
         node = next;
@@ -102,7 +102,7 @@ void List_DeleteMatch(List* list, bool (*match)(void*))
 
 void List_PushFront(List* list, void* data)
 {
-    ListNode* node = ListNode_Construct(data);
+    Node* node = Node_Construct(data);
     node->next = list->head;
     list->head = node;
     list->size += 1;
@@ -119,14 +119,14 @@ void* List_InspectFront(List* list)
 
 void List_MapForward(List* list, void (*func)(void*))
 {
-    for(ListNode* node = list->head; node; node = node->next)
+    for(Node* node = list->head; node; node = node->next)
         func(node->data);
 }
 
 void* List_PopFront(List* list)
 {
     void* data = List_InspectFront(list);
-    ListNode* next = list->head->next;
+    Node* next = list->head->next;
     free(list->head);
     list->head = next;
     list->head->prev = NULL;
@@ -144,7 +144,7 @@ void* List_InspectBack(List* list)
 void* List_PopBack(List* list)
 {
     void* data = List_InspectBack(list);
-    ListNode* prev = list->tail->prev;
+    Node* prev = list->tail->prev;
     free(list->tail);
     list->tail = prev;
     list->tail->next = NULL;
@@ -156,6 +156,6 @@ void* List_PopBack(List* list)
 
 void List_MapBackward(List* list, void (*func)(void*))
 {
-    for(ListNode* node = list->tail; node; node = node->prev)
+    for(Node* node = list->tail; node; node = node->prev)
         func(node->data);
 }
